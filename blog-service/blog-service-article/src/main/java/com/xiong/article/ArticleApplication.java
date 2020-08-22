fea23d39d2d9ba59e3f2348cfa57154fdb97af1b
yan129 <1291248490@qq.com>
2020-08-22 18:28:16 +0800

    整合公共类，生成器生成实体

--- blog-service/blog-service-article/src/main/java/com/xiong/article/ArticleApplication.java
+++ blog-service/blog-service-article/src/main/java/com/xiong/article/ArticleApplication.java
@@ -3,9 +3,11 @@ package com.xiong.article;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.xiong.article")
+@ComponentScan(basePackages = "com.xiong")
+@MapperScan(basePackages = "com.xiong.article.mapper")
 public class ArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
